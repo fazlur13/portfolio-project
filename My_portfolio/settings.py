5a4f4b6b7b43ac422e6149e5267b9df12ccf2f6e
fazlur13 <fazlur.rah09@gmail.com>
2019-07-16 02:36:08 +0530

    Running Job mode

--- My_portfolio/settings.py
+++ My_portfolio/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'My_portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfoliodb',
+        'USER': 'postgres',
+        'PASSWORD': 'hazel7kaya',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
